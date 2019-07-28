2caa4955b30ca43d3fdefc4dd5095dc77ccd77ca
vms <michail.vms@gmail.com>
2019-07-28 14:36:49 +0300

    apply fmt

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@
 
 // TODO: add some docs
 
-use parity_wasm;
-use parity_wasm::elements::{NameSection, ModuleNameSubsection, Serialize};
 use clap::{App, AppSettings, Arg, SubCommand};
 use exitfailure::ExitFailure;
 use failure::err_msg;
+use parity_wasm;
+use parity_wasm::elements::{ModuleNameSubsection, NameSection, Serialize};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
@@ -35,8 +35,7 @@ fn show_module_name_subcommand<'a, 'b>() -> App<'a, 'b> {
     let arg = &[Arg::with_name(MODULE_PATH)
         .required(true)
         .takes_value(true)
-        .help("path to the wasm file")
-    ];
+        .help("path to the wasm file")];
 
     SubCommand::with_name("show")
         .about("Show Wasm file name")
@@ -73,51 +72,54 @@ fn main() -> Result<(), ExitFailure> {
         ("show", Some(arg)) => {
             let module_path = arg.value_of(MODULE_PATH).unwrap();
 
-            let module = parity_wasm::deserialize_file(module_path).expect("Error while deserializing file");
+            let module =
+                parity_wasm::deserialize_file(module_path).expect("Error while deserializing file");
             let module = module.parse_names().expect("Error while parsing names");
             let name_section = module.names_section();
 
             let no_module_name: &str = "<no-name>";
 
             let module_name = match name_section {
-                Some(name_section) => {
-                    match name_section.module() {
-                        Some(name_section) => name_section.name(),
-                        None => no_module_name,
-                    }
-                }
+                Some(name_section) => match name_section.module() {
+                    Some(name_section) => name_section.name(),
+                    None => no_module_name,
+                },
                 None => no_module_name,
             };
 
             println!("The module name is {}", module_name);
             Ok(())
-        },
+        }
 
         ("set", Some(args)) => {
             let module_path = args.value_of(MODULE_PATH).unwrap();
             let new_module_name = args.value_of(NEW_MODULE_NAME).unwrap();
 
-            let module = parity_wasm::deserialize_file(module_path).expect("Error while deserializing file");
+            let module =
+                parity_wasm::deserialize_file(module_path).expect("Error while deserializing file");
             let mut module = module.parse_names().expect("Error while parsing names");
             let name_section = module.names_section_mut();
 
             let new_module_name_subsection = ModuleNameSubsection::new(new_module_name);
             match name_section {
-                Some(name_section) =>
-                    *name_section.module_mut() = Some(new_module_name_subsection),
+                Some(name_section) => *name_section.module_mut() = Some(new_module_name_subsection),
                 None => {
-                    let name_section = NameSection::new(Some(new_module_name_subsection), None, None);
+                    let name_section =
+                        NameSection::new(Some(new_module_name_subsection), None, None);
 
                     let mut buffer = vec![];
-                    name_section.serialize(&mut buffer).expect("Error while serializing name section");
+                    name_section
+                        .serialize(&mut buffer)
+                        .expect("Error while serializing name section");
 
                     module.set_custom_section("name", buffer);
                 }
             };
 
-            parity_wasm::serialize_to_file(module_path, module).expect("Error while serializing file");
+            parity_wasm::serialize_to_file(module_path, module)
+                .expect("Error while serializing file");
             Ok(())
-        },
+        }
 
         c => Err(err_msg(format!("Unexpected command: {}", c.0)))?,
     }
